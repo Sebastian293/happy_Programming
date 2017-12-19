e026ec20544eaa60fe4d7aa49a549f6185c7019c
sebastian.rau <sebastian.rau@student.fh-kiel.de>
2017-12-19 17:49:44 +0100

    Ersten Testfall geschrieben für den Fall das alles gut geht.

--- src/main/java/edu/morghath/service/FizzBuzzService.java
+++ src/main/java/edu/morghath/service/FizzBuzzService.java
@@ -2,6 +2,14 @@ package edu.morghath.service;
 
 public class FizzBuzzService {
 
+	/**
+	 * Returns a differnt String depending on the input parameter.
+	 * 
+	 * @param n
+	 *            the parameter you want to test
+	 * @return Fizz if n is divideble by 3, Buzz if n is divideble by 5, FizzBizz if
+	 *         both, else n
+	 */
 	public static String fizzBuzz(int n) {
 		return Integer.toString(n);
 	}
---
+++ src/test/java/edu/morghath/service/FizzBuzzServiceTest.java
@@ -0,0 +1,21 @@
+package edu.morghath.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FizzBuzzServiceTest {
+
+	@Test
+	public void fizzBuzzSimpleValuesTest() throws Exception {
+		String simpleNummber = FizzBuzzService.fizzBuzz(1);
+		String numberDivBy3 = FizzBuzzService.fizzBuzz(36);
+		String numberDivBy5 = FizzBuzzService.fizzBuzz(80);
+		String numberDivBy3and5 = FizzBuzzService.fizzBuzz(45);
+		
+		assertTrue(simpleNummber == "1");
+		assertTrue(numberDivBy3 == "Fizz");
+		assertTrue(numberDivBy5 == "Buzz");
+		assertTrue(numberDivBy3and5 == "FizzBuzz");
+	}
+}
