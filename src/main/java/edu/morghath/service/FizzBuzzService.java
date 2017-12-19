7426eccec2854877019d90bece913100cf77fd63
sebastian.rau <sebastian.rau@student.fh-kiel.de>
2017-12-19 18:42:14 +0100

    Test angepasst so das er richtig Testet und umsetzung in fizzBuzz

--- src/main/java/edu/morghath/service/FizzBuzzService.java
+++ src/main/java/edu/morghath/service/FizzBuzzService.java
@@ -1,28 +1,38 @@
 package edu.morghath.service;
 
 public class FizzBuzzService {
+	private static final int BOTTOM_BORDER = 0;
+	private static final int TOP_BORDER = 100;
 
 	/**
-	 * Takes an Integer between 0 and 100 and returns 'Fizz', 'Buzz', 'FizzBuzz' or the Integer.
+	 * Takes an Integer between 0 and 100 and returns 'Fizz', 'Buzz', 'FizzBuzz' or
+	 * the Integer.
 	 * 
 	 * @param n
 	 *            the parameter you want to test. (Must be between 0 and 100)
 	 * @return Fizz if n is divideble by 3, Buzz if n is divideble by 5, FizzBizz if
-	 *         both, else n
+	 *         both, else n.
+	 * @throws Exception
+	 *             if n is not betwenn the BOTTOM_BORDER and TOP_BORDER
 	 */
-	public static String fizzBuzz(int n) {
-		if(n % 5 == 0 && n % 3 == 0){
+	public static String fizzBuzz(int n) throws Exception {
+		if (n < BOTTOM_BORDER || n > TOP_BORDER) {
+			throw new Exception(
+					"Es dürfen nur Zahlen im Intervall von[" + BOTTOM_BORDER + "," + TOP_BORDER + "] verwendet werden");
+		}
+
+		if (n % 5 == 0 && n % 3 == 0) {
 			return "FizzBuzz";
 		}
-		
-		if(n % 3 == 0) {
+
+		if (n % 3 == 0) {
 			return "Fizz";
 		}
-		
-		if(n % 5 == 0) {
-			return  "Buzz";
+
+		if (n % 5 == 0) {
+			return "Buzz";
 		}
-		
+
 		return Integer.toString(n);
 	}
 }
--- src/test/java/edu/morghath/service/FizzBuzzServiceTest.java
+++ src/test/java/edu/morghath/service/FizzBuzzServiceTest.java
@@ -2,12 +2,18 @@ package edu.morghath.service;
 
 import static org.junit.Assert.*;
 
+import org.hamcrest.core.StringStartsWith;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class FizzBuzzServiceTest {
-	private static final int BOTTOMBORDER = 0;
-	private static final int TOPBORDER = 100;
+	private static final int BOTTOM_BORDER = 0;
+	private static final int TOP_BORDER = 100;
 
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+	
 	@Test
 	public void fizzBuzzSimpleValuesTest() throws Exception {
 		String simpleNummber = FizzBuzzService.fizzBuzz(1);
@@ -15,22 +21,32 @@ public class FizzBuzzServiceTest {
 		String numberDivBy5 = FizzBuzzService.fizzBuzz(80);
 		String numberDivBy3and5 = FizzBuzzService.fizzBuzz(45);
 		
+		String bottomBorder = FizzBuzzService.fizzBuzz(BOTTOM_BORDER);
+		String topBorder = FizzBuzzService.fizzBuzz(TOP_BORDER);
+		
 		assertTrue(simpleNummber.equals("1"));
 		assertTrue(numberDivBy3 == "Fizz");
 		assertTrue(numberDivBy5 == "Buzz");
 		assertTrue(numberDivBy3and5 == "FizzBuzz");
+		
+		assertTrue(bottomBorder == "FizzBuzz");
+		assertTrue(topBorder == "Buzz");
 	}
 	
 	@Test
-	public void fizzBuzzBetweenBordersTest() throws Exception {
-		String outOfBorderBottom = FizzBuzzService.fizzBuzz(BOTTOMBORDER -1);
-		String bottomBorder = FizzBuzzService.fizzBuzz(BOTTOMBORDER);
-		String topBorder = FizzBuzzService.fizzBuzz(TOPBORDER);
-		String outOfTopBorder = FizzBuzzService.fizzBuzz(TOPBORDER + 1);
+	public void fizzBuzzOutOfBottomBorderTest() throws Exception {
+		thrown.expect(Exception.class);
+		thrown.expectMessage("Es dürfen nur Zahlen im Intervall von[" + BOTTOM_BORDER + "," + TOP_BORDER + "] verwendet werden");
 		
-		assertTrue(outOfBorderBottom == null);
-		assertTrue(bottomBorder == "FizzBuzz");
-		assertTrue(topBorder == "Buzz");
-		assertTrue(outOfTopBorder == null);
+		FizzBuzzService.fizzBuzz(BOTTOM_BORDER -1);
+	}
+	
+	
+	@Test
+	public void fizzBuzzOutOfTopBorderTest() throws Exception {
+		thrown.expect(Exception.class);
+		thrown.expectMessage("Es dürfen nur Zahlen im Intervall von[" + BOTTOM_BORDER + "," + TOP_BORDER + "] verwendet werden");
+
+		FizzBuzzService.fizzBuzz(TOP_BORDER + 1);
 	}
 }
